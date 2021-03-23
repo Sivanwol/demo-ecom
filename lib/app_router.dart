e1811f572fe36e6e075ea35eec8e1b60442e2eca
Sivan Wolberg <sivan@wolberg.pro>
2021-03-23 13:12:42 +0200

    add deps of state mng as well support for the routing system

---
+++ lib/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:auto_route/auto_route.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    // AutoRoute(page: BookListPage, initial: true),
+  ],
+)
+class AppRouter {}
