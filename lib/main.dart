4bd5e950a1b792d90a1d33ebe0d76dde54168003
Sivan Wolberg <sivan@wolberg.pro>
2021-03-24 22:08:45 +0200

    remove get_it package

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,7 @@ import 'dart:async';
 
 import 'package:demo_ecom/common/app/application.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-import 'main.config.dart';
-// ignore: avoid_void_async
-final getIt = GetIt.instance;
-
-@InjectableInit(
-  generateForDir: ['lib/generated'],
-  initializerName: r'$initGetIt', // default
-  preferRelativeImports: true, // default
-  usesNullSafety: true,
-  asExtension: false, // default
-)
-void configureDependencies() => $initGetIt(getIt);
 void main() async {
-  configureDependencies();
   WidgetsFlutterBinding.ensureInitialized();
   runZonedGuarded(() {
     runApp(Application());
