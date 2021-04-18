ab71af0cb3930ba2fe24753e2dc792a0eff33b9f
Sivan Wolberg <sivan@wolberg.pro>
2021-04-18 18:43:33 +0300

    something odd the firebase not connected to the emulator for what ever reason as well remove the reset i need the user see what he did wrong  #CU-7fz309

--- lib/common/utils/firebase_utils.dart
+++ lib/common/utils/firebase_utils.dart
@@ -6,7 +6,7 @@ Future<FirebaseApp> setupFirebase() async {
   var app = await Firebase.initializeApp();
   if (ApplicationConfig.debug_mode && ApplicationConfig.firebase_emulator) {
     FirebaseAuth.instance
-        .useEmulator('${ApplicationConfig.firebase_address}:9099');
+        .useEmulator('http://${ApplicationConfig.firebase_address}:9099');
     // Firestore.instance.settings(
     //   host: host,
     //   sslEnabled: false,
