28e9c31d4773118b668e688abf7c5f21618749b9
Sivan Wolberg <sivan@wolberg.pro>
2021-05-12 20:23:49 +0300

    remove firebase functions system
    CU-8624j2

--- functions/src/common/collections.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    collectionEntities: "entities",
-    collectionEntityHours: "entity_hours"
-}
--- functions/src/requests/DeleteEntitiesRequest.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from "class-validator";
-
-export class DeleteEntitiesRequest {
-    @IsString({each: true})
-    public entities_ids: string[];
-}
